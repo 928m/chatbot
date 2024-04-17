9db23bbc784810df58f846a68a75a9497daeba97
jihee min <jihee9289@gmail.com>
2024-04-18 00:17:09 +0900

    feat: chat post api 호출

--- src/app/components/Chat.tsx
+++ src/app/components/Chat.tsx
@@ -6,27 +6,49 @@ import {
   Spacing,
 } from "@/app/components";
 
+import axios from "axios";
+import { useState } from "react";
+
+type ChatMessage = {
+  type: "USER" | "BOT";
+  value: string;
+};
+
 export default function Chat() {
+  const [chatMessages, setChatMessages] = useState<ChatMessage[]>([]);
+  const [loading, setLoading] = useState<boolean>(false);
+
+  const loadChat = async (value: string) => {
+    try {
+      setLoading(true);
+      const { data } = await axios.post("/api", { message: value });
+
+      setChatMessages((prev) => [...prev, { type: "BOT", value: data.result }]);
+    } catch (err: any) {
+      console.error(err.message);
+      alert("Failed to load chat");
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleClickSubmit = (value: string) => {
+    setChatMessages((prev) => [...prev, { type: "USER", value }]);
+    loadChat(value);
+  };
+
   return (
     <ChatBox>
       <MessageBox>
-        <Message type="USER">
-          he Internet. It uses a dictionary of over 200 Latin words, combined
-          with a handful of model sentence structures, to generate Lorem Ipsum
-          which looks reasonable. The generated Lorem Ipsum is therefore always
-          free from repetition, injected humour, or non-characteristic words
-          etc. 5 paragraphs wor
-        </Message>
-        <Spacing size={20} />
-        <Message type="BOT">
-          he Internet. It uses a dictionary of over 200 Latin words, combined
-          with a handful of model sentence structures, to generate Lorem Ipsum
-          which looks reasonable. The generated Lorem Ipsum is therefore always
-          free from repetition, injected humour, or non-characteristic words
-          etc. 5 paragraphs wor
-        </Message>
+        {chatMessages.map((message, index) => (
+          <div key={index}>
+            <Message type={message.type}>{message.value}</Message>
+            <Spacing size={20} />
+          </div>
+        ))}
+        {loading && <Message type="BOT">Loading...</Message>}
       </MessageBox>
-      <InputField />
+      <InputField onClickSubmit={handleClickSubmit} />
     </ChatBox>
   );
 }
