9fa82f39857315c78eeafb4b440060a1d7ea70e1
jihee min <jihee9289@gmail.com>
2024-04-23 13:51:58 +0900

    refactor: export 파일 추가

---
+++ index.ts
@@ -0,0 +1,10 @@
+import {
+  BotLoading,
+  Button,
+  ChatBox,
+  InputField,
+  Message,
+  MessageBox,
+} from "@/app/components";
+
+export { BotLoading, Button, ChatBox, InputField, Message, MessageBox };
