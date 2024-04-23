1c5a29be818bfe01f93b3cfc4f7cef28849f5de7
jihee min <jihee9289@gmail.com>
2024-04-23 13:13:46 +0900

    refactor: input 컴포넌트 리팩토링

--- src/app/components/Input.tsx
+++ src/app/components/Input.tsx
@@ -1,13 +1,32 @@
-export default function Input(
-  props: React.InputHTMLAttributes<HTMLInputElement>
-) {
+interface InputProps {
+  type?: "text" | "password" | "email" | "number" | "search";
+  placeholder?: string;
+  value?: string | number;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+}
+
+export default function Input({
+  type = "text",
+  placeholder = "",
+  value = "",
+  onChange = () => {},
+  onFocus = () => {},
+  onBlur = () => {},
+  disabled = false,
+}: InputProps) {
   return (
-    <div className="w-full">
-      <input
-        type="text"
-        className="flex w-full h-[40px] px-[10px] bg-transparent"
-        {...props}
-      />
-    </div>
+    <input
+      className="flex w-full h-[40px] px-[10px] bg-transparent"
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      disabled={disabled}
+    />
   );
 }
